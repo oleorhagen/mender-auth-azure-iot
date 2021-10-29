e6953ad57e209d8301c5f6ba223bcfe54572ecac
Ole Petter <ole.orhagen@northern.tech>
2021-10-29 16:14:52 +0200

    Start testing out the azure-iot-device package
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- src/daemon/daemon.py
+++ src/daemon/daemon.py
@@ -12,12 +12,60 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 import time
+import os
+import asyncio
+from azure.iot.device.aio import IoTHubDeviceClient
 
 
-def main():
+
+async def send_message():
+    # Fetch the connection string from an enviornment variable
+    # TODO - get this from the configuration file
+    conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
+
+    # Create instance of the device client using the authentication provider
+    device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
+
+    # Connect the device client.
+    await device_client.connect()
+
+    # Send a single message
+    print("Sending message...")
+    await device_client.send_message("This is a message that is being sent")
+    print("Message successfully sent!")
+
+    # finally, shut down the client
+    await device_client.shutdown()
+
+async def get_message():
+
+    conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
+    device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
+
+    # connect the client.
+    device_client.connect()
+
+    # get the twin
+    twin = device_client.get_twin()
+    print("Twin document:")
+    print("{}".format(twin))
+
+    # Finally, shut down the client
+    device_client.shutdown()
+
+async def main():
+
     while True:
+
+        # TODO - get once an hour
+        # TODO - get the device identity, and send it with the data
+
+        get_message()
+
+        send_message()
+
         time.sleep(10)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
